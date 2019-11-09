f8e025c2fdb131f0bdb94ff8ff7a9c8ebb9f8412
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-11-09 12:38:12 -0800

    Workin on filtering items, need to figure how to match items

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 console.log('connected...');
 
-// Hate Typing Console Log
-const log = (item) => {
-    console.log(item);
-}
-
 // Define UI Variables
 const form = document.querySelector('#task-form');
 const tasks = document.querySelector('.collection');
@@ -16,20 +11,33 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 
-// FUNCTIONS
+/******************************************
+    FUNCTIONS
+******************************************/
+
 function loadEventListeners() {
     // Add Task Form Even Listener
     form.addEventListener('submit', addTask);
+
+    // Use evend deligation to drip down
+    tasks.addEventListener('click', removeTask);
+
+    // Add Remove All Task Event Listener
+    clearBtn.addEventListener('click', removeAllTasks);
+
+    // Filter Tasks Event Listener 
+    filterInput.addEventListener('keyup', filterTasks);
 }
-    
+
+/******************************************
+    Add Task
+******************************************/
 function addTask(e) {
     e.preventDefault();
 
     if ( taskInput.value === '') {
         alert('Please Add a Task!');
     } else {
-        log(taskInput.value);
-
         // Create li Element
         const task = document.createElement('li');
         task.className = 'collection-item';
@@ -62,4 +70,77 @@ function addTask(e) {
     
 
     console.log( 'Task Add Clicked');
+}
+
+/******************************************
+    Remove Task
+******************************************/
+
+function removeTask(e) {
+    const selected = e.target.parentElement;
+
+    if (selected.classList.contains('delete-item')) {
+
+        const message =`Are You Sure you want to remove ${selected.parentElement.textContent}.`;
+        if ( confirm(message) ) {
+            // Remove li from the ul
+            selected.parentElement.remove();
+        }     
+
+    }
+
+}
+
+
+/******************************************
+    Remove All Tasks
+******************************************/
+function removeAllTasks () {
+
+    /*
+    if ( tasks.children.length > 0 ) {            
+        if ( confirm('You will destroy everything.') ) {
+
+        
+            // Convert HTML collection to array
+            const newArray = Array.from(tasks.children);
+
+            newArray.forEach( (item) => {
+                item.remove();
+            });
+            
+
+        }    
+    } 
+    */
+
+    while (tasks.firstChild) {
+        tasks.removeChild(tasks.firstChild);
+    }
+
+}
+
+/******************************************
+    Filter Tasks
+******************************************/
+function filterTasks(e) {
+    
+    console.log('filter');
+    const text = e.target.value.toLowerCase();
+    
+    // No need to convert to array when use querySelectorAll because returns node list not HTML collection
+    const allItems = document.querySelectorAll('.collection-item');
+    allItems.forEach( (item) => {
+
+        const currentItemText = item.firstChild.textContent;
+
+        if (currentItemText.toLowerCase().indexOf(text) != -1 ) {
+            tasks.style.display = "block";
+        } else {
+            tasks.style.display = "none";
+
+        }
+    })   
+    // 
+
 }
